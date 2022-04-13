7117ef02865cd7295de37b504ac9347923ef0645
Yan Luo <yluo@ca.ibm.com>
2022-04-12 21:28:56 -0400

    updated push and pop

--- leetcode/heap/heapq.py
+++ leetcode/heap/heapq.py
@@ -1,5 +1,5 @@
 import heapq as heap
-from myheap import Heap
+from myheap import MinHeap
 import unittest
 
 '''
@@ -10,22 +10,35 @@ Example of a min heap
 6 5 4  8
 '''
 
-arr = [1, 5, 2, 6, 3, 4, 8]
-arr2 = [1, 5, 2, 6, 3, 4, 8]
-# 5, 2, 9
-print(f"Before heapify {arr}")
-heap.heapify(arr)
-heap2 = Heap()
-heap2.heaplify(arr2)
-print(f"After heapify")
-print(arr)
-print(arr2)
+min_heap = MinHeap()
 
-print("Sorting arr 2 descending")
-heap2.heap_sort(arr2)
-print(arr2)
-for i in range(len(arr)):
-    print(heap.heappop(arr), end="-")
-print("")
-for i in range(len(arr2)):
-    print(heap2.heappop(arr2), end="-")
+def test_heaplify():
+    arr = [1, 5, 2, 6, 3, 4, 8]
+    arr2 = [1, 5, 2, 6, 3, 4, 8]
+    # 5, 2, 9
+    print(f"Before heapify {arr}")
+    heap.heapify(arr)
+    print(f"After heapify")
+    print(arr)
+    print(arr2)
+    print("Sorting arr 2 descending")
+    min_heap.heap_sort(arr2)
+    print(arr2)
+
+def test_push_pop():
+    arr = [1, 5, 2, 6, 3, 4, 8]
+    heap1 = []
+    heap2 = []
+    for num in arr:
+        heap.heappush(heap1, num)
+        min_heap.heappush(heap2, num)
+    print(heap1)
+    print(heap2)
+
+    for i in range(len(arr)):
+        print(heap.heappop(heap1), end="-")
+    print("\n")
+    for i in range(len(arr)):
+        print(min_heap.heappop(heap2), end="-")
+
+test_push_pop()
--- leetcode/heap/myheap.py
+++ leetcode/heap/myheap.py
@@ -1,13 +1,14 @@
-class Heap:
+class MinHeap:
 
     def __init__(self):
         pass
 
     '''
+    Topdown to make sure the root of the tree if satisfied
     one underscore as a private method
     Swap the element
     '''
-    def _swap(self, idx, arr, length):
+    def _topdown(self, idx, arr, length):
         min_idx = idx
         left = 2 * idx + 1
         right = 2 * idx + 2
@@ -23,7 +24,26 @@ class Heap:
             # swap with child with min value
             arr[idx], arr[min_idx] = arr[min_idx], arr[idx]
             # Continue with the min child
-            self._swap(min_idx, arr, length)
+            self._topdown(min_idx, arr, length)
+
+    '''
+    Bottom up for inserting
+    Insert the element to the end
+    If parent is larger than end, swap with parent till root
+    '''
+    def _bottomup(self, idx, arr):
+        min_idx = idx
+        parent = idx // 2
+
+        if parent >= 0 and arr[parent] > arr[min_idx]:
+            min_idx = parent
+
+        # If child has the min value
+        if min_idx != idx:
+            # swap with child with min value
+            arr[idx], arr[min_idx] = arr[min_idx], arr[idx]
+            # Continue with the min child
+            self._bottomup(min_idx, arr)
 
     '''
     Heaplify the array in-place
@@ -37,7 +57,7 @@ class Heap:
         start_idx = n_len // 2 - 1
         # Starting with the last non-leaf node, heapify the array
         for i in range(start_idx, -1, -1):
-            self._swap(i, arr, n_len)
+            self._topdown(i, arr, n_len)
 
     def heap_sort(self, arr:[]):
         self.heaplify(arr)
@@ -49,12 +69,24 @@ class Heap:
             # Need to swap it down
             # make sure the any subtree satisfies the heap properties
             # and the last parts are sorted descending
-            self._swap(0, arr, i)
+            self._topdown(0, arr, i)
 
     '''
     Pop the min element
     '''
     def heappop(self, arr: []):
-        # pop the last node (min)
-        # if you pop it n times then the array is sorted ascending
-        return arr.pop()
+        n = len(arr)
+        arr[n-1], arr[0] = arr[0], arr[n-1]
+        # Top down to make sure the root satisfies heap properties
+        res = arr.pop()
+        n = len(arr)
+        self._topdown(0, arr, n)
+        return res
+
+    def heappush(self, arr: [], num):
+        # First add the num to the end of list
+        arr.append(num)
+        n = len(arr)
+        # Then swap it to the first
+        # Make sure root satisfy heap properties
+        self._bottomup(n-1, arr)
